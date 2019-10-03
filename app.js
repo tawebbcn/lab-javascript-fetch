a4d8cf096964eaed970c403928647430461f5b8f
Fuvanbuh <gerardfunes@telefonica.net>
2019-10-03 23:23:07 +0200

    ejercicio 1 y 2 lab

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-'use strict';
+//'use strict';
 
-function main() {
-  console.log('Your code goes here');
+
+
+async function main() {
+  let perrosTotal = await fetch ('https://dog.ceo/api/breeds/image/random')
+  let imgRandom = await perrosTotal.json();
+  console.log(imgRandom);
   console.log('Happy coding!');
+  const container = document.querySelector('.container')
+  container.innerHTML=`<img src="${imgRandom.message}" alt="foto de perro">`
 };
 
-window.addEventListener('load', main);
+
+
+async function money (){
+  const moneyApi = await fetch('https://api.exchangeratesapi.io/latest')
+  const allMoney = await moneyApi.json();
+  const select = document.createElement('select')
+  for(const key in allMoney.rates){
+  let option = document.createElement('option')
+  option.innerHTML = `${key}: ${allMoney.rates[key]}`
+  select.appendChild(option);
+}
+document.querySelector('.pie').appendChild(select)
+}
+
+window.addEventListener('load', main)
+window.addEventListener('load', money);
